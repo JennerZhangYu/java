198f10a0278531cd5a1ebfea35dc950f95051edf
JennerZhangYu <103395217+JennerZhangYu@users.noreply.github.com>
2022-05-04 09:58:33 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+
+
+
+    text=prompt ("please enter a value")
+    if (text==="student")  {window.location.href="https://jennerzhangyu.github.io/new/introduce.html";
+        
+    }
+    else if    (text==="parent")  {{window.location.href="https://jennerzhangyu.github.io/new/index.html";
+        
+}
+    }
+
